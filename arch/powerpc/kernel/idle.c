e8ca5e58e48bbcd42532cc27f43ca48094349e8d
Sergei Shtylyov <sshtylyov@ru.mvista.com>
2007-05-17 21:46:46 +0400

    PowerPC: enable HRT and dynticks support
    Enable HRT and dynamic ticks support for PowerPC.
    
    Signed-off-by: Sergei Shtylyov <sshtylyov@ru.mvista.com>

--- arch/powerpc/kernel/idle.c
+++ arch/powerpc/kernel/idle.c
@@ -24,6 +24,7 @@
 #include <linux/smp.h>
 #include <linux/cpu.h>
 #include <linux/sysctl.h>
+#include <linux/tick.h>
 
 #include <asm/system.h>
 #include <asm/processor.h>
@@ -59,6 +60,8 @@ void cpu_idle(void)
 
 	set_thread_flag(TIF_POLLING_NRFLAG);
 	while (1) {
+		tick_nohz_stop_sched_tick();
+
 		while (!need_resched() && !cpu_should_die()) {
 			ppc64_runlatch_off();
 
@@ -92,6 +95,9 @@ void cpu_idle(void)
 		ppc64_runlatch_on();
 		if (cpu_should_die())
 			cpu_die();
+
+		tick_nohz_restart_sched_tick();
+
 		preempt_enable_no_resched();
 		schedule();
 		preempt_disable();

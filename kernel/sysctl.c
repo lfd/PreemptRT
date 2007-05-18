1c22f948a0b28bbef0be0eb4cd82d7b400ae55b9
Paul Mundt <lethal@linux-sh.org>
2007-05-18 13:45:59 +0900

    futex_performance_hack sysctl build fix
    -rt adds a futex_performance_hack sysctl, which is only defined if
    kernel/futex.c is built in. This fixes the build in the CONFIG_FUTEX=n
    case.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>
    
    --
    
     kernel/sysctl.c |    2 ++
     1 file changed, 2 insertions(+)

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -359,6 +359,7 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 #endif
+#ifdef CONFIG_FUTEX
 	{
 		.ctl_name	= CTL_UNNUMBERED,
 		.procname	= "futex_performance_hack",
@@ -367,6 +368,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= &proc_dointvec,
 	},
+#endif
 	{
 		.ctl_name	= CTL_UNNUMBERED,
 		.procname	= "prof_pid",

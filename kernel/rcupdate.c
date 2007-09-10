9073e254f4b2b1a9934550a2bae72a382b77f9b6
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2007-09-10 11:35:25 -0700

    RCU: synchronize_sched() workaround for CPU hotplug
    Work in progress, not for inclusion.
    
    The combination of CPU hotplug and PREEMPT_RCU has resulted in deadlocks
    due to the migration-based implementation of synchronize_sched() in -rt.
    This experimental patch maps synchronize_sched() back onto Classic RCU,
    eliminating the migration, thus hopefully also eliminating the deadlocks.
    It is not clear that this is a good long-term approach, but it will at
    least permit people doing CPU hotplug in -rt kernels additional wiggle
    room in their design and implementation.
    
    The basic approach is to cause the -rt kernel to incorporate rcuclassic.c
    as well as rcupreempt.c, but to #ifdef out the conflicting portions of
    rcuclassic.c so that only the code needed to implement synchronize_sched()
    remains in a PREEMPT_RT build.  Invocations of grace-period detection from
    the scheduling-clock interrupt go to rcuclassic.c, which then invokes
    the corresponding functions in rcupreempt.c (with _rt suffix added to
    keep the linker happy).  Also applies the RCU_SOFTIRQ to classic RCU.
    The bulk of this patch just moves code around, but likely increases
    scheduling-clock latency.
    
    If this patch does turn out to be the right approach, the #ifdefs in
    kernel/rcuclassic.c might be dealt with.  ;-)  At current writing, Gautham
    Shenoy's most recent CPU-hotplug fixes seem likely to obsolete this patch
    (which would be a very good thing indeed!).  If this really pans out,
    this portion of the patch will vanish during the forward-porting process.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org> (for RCU_SOFTIRQ)
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcupdate.c
+++ kernel/rcupdate.c
@@ -52,6 +52,7 @@ struct rcu_synchronize {
 	struct completion completion;
 };
 
+DEFINE_PER_CPU(int, rcu_data_passed_quiesc);
 static DEFINE_PER_CPU(struct rcu_head, rcu_barrier_head) = {NULL};
 static atomic_t rcu_barrier_cpu_count;
 static DEFINE_MUTEX(rcu_barrier_mutex);
@@ -87,6 +88,22 @@ void synchronize_rcu(void)
 	wait_for_completion(&rcu.completion);
 }
 EXPORT_SYMBOL_GPL(synchronize_rcu);
+
+#ifdef CONFIG_PREEMPT_RCU
+
+/*
+ * Map synchronize_sched() to the classic RCU implementation.
+ */
+void __synchronize_sched(void)
+{
+	struct rcu_synchronize rcu;
+
+	init_completion(&rcu.completion);
+	call_rcu_classic(&rcu.head, wakeme_after_rcu);
+	wait_for_completion(&rcu.completion);
+}
+EXPORT_SYMBOL_GPL(__synchronize_sched);
+#endif /* #ifdef CONFIG_PREEMPT_RCU */
 
 static void rcu_barrier_callback(struct rcu_head *notused)
 {
@@ -130,9 +147,4 @@ void rcu_barrier(void)
 	rcu_read_unlock();
 	wait_for_completion(&rcu_barrier_completion);
 	mutex_unlock(&rcu_barrier_mutex);
-}
-
-void __init rcu_init(void)
-{
-	__rcu_init();
 }

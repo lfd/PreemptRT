eccb4b27cdb08185bb480446657d867a3db0aadc
Gregory Haskins <ghaskins@novell.com>
2008-02-26 12:14:27 -0500

    fix oops in root-domain code during repartitioning
        [ The following text is in the "utf-8" character set. ]
        [ Your display is set for the "iso-8859-1" character set.  ]
        [ Some characters may be displayed incorrectly. ]
    
    we cannot kfree while in_atomic in -rt, and we currently hold the
    (raw_spinlock_t)rq->lock while we try.  So defer the operation until
    we are out of the critical section.
    
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -6910,6 +6910,7 @@ static void rq_attach_root(struct rq *rq, struct root_domain *rd)
 {
 	unsigned long flags;
 	const struct sched_class *class;
+	struct root_domain *reap = NULL;
 
 	spin_lock_irqsave(&rq->lock, flags);
 
@@ -6925,7 +6926,7 @@ static void rq_attach_root(struct rq *rq, struct root_domain *rd)
 		cpu_clear(rq->cpu, old_rd->online);
 
 		if (atomic_dec_and_test(&old_rd->refcount))
-			kfree(old_rd);
+			reap = old_rd;
 	}
 
 	atomic_inc(&rd->refcount);
@@ -6941,6 +6942,10 @@ static void rq_attach_root(struct rq *rq, struct root_domain *rd)
 	}
 
 	spin_unlock_irqrestore(&rq->lock, flags);
+
+	/* Don't try to free the memory while in-atomic() */
+	if (unlikely(reap))
+		kfree(reap);
 }
 
 static void init_rootdomain(struct root_domain *rd)

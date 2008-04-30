26abf9797642f2c255111a8d4d31f1839b2b8671
Thomas Gleixner <tglx@linutronix.de>
2008-04-30 15:01:10 +0200

    rt: move update_wall_time back to do timer
    Heavy networking or high load rt tasks can starve the timer
    softirq. This can result in long loops in update_wall_time() once the
    timer softirq gets hold of the CPU again. This code runs with
    interrupts disabled and xtime lock write locked, so it can introduce
    pretty long latencies. Move update_wall_time() back into do_timer, so
    we avoid the looping and have a small but constant length irq off
    section every tick.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/timer.c
+++ kernel/timer.c
@@ -1288,7 +1288,6 @@ static inline void update_times(void)
 	ticks = jiffies - last_tick;
 	if (ticks) {
 		last_tick += ticks;
-		update_wall_time();
 		calc_load(ticks);
 	}
 	write_sequnlock_irqrestore(&xtime_lock, flags);
@@ -1318,6 +1317,7 @@ static void run_timer_softirq(struct softirq_action *h)
 void do_timer(unsigned long ticks)
 {
 	jiffies_64 += ticks;
+	update_wall_time();
 }
 
 #ifdef __ARCH_WANT_SYS_ALARM

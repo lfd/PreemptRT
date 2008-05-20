8a44059a3c901f947690e19c6d6a25a5a458e6bd
Gregory Haskins <ghaskins@novell.com>
2008-05-20 10:49:20 -0400

    sched: make task->oncpu available in all configurations
        [ The following text is in the "utf-8" character set. ]
        [ Your display is set for the "iso-8859-1" character set.  ]
        [ Some characters may be displayed incorrectly. ]
    
    We will use this later in the series to eliminate the need for a function
    call.
    
    [ Steven Rostedt: added task_is_current function ]
    
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1088,9 +1088,7 @@ struct task_struct {
 	int lock_depth;		/* BKL lock depth */
 
 #ifdef CONFIG_SMP
-#ifdef __ARCH_WANT_UNLOCKED_CTXSW
 	int oncpu;
-#endif
 #endif
 
 	int prio, static_prio, normal_prio;
@@ -2370,7 +2368,12 @@ static inline void migration_init(void)
 #define TASK_SIZE_OF(tsk)	TASK_SIZE
 #endif
 
-extern int task_is_current(struct task_struct *task);
+#ifdef CONFIG_SMP
+static inline int task_is_current(struct task_struct *task)
+{
+	return task->oncpu;
+}
+#endif
 
 #define TASK_STATE_TO_CHAR_STR "RMSDTtZX"
 

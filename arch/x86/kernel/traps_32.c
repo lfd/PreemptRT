22e76f534f85b204ffd37788df8a3b4090b9916f
Thomas Gleixner <tglx@linutronix.de>
2008-06-19 19:24:14 +0200

    fix-tracer-wreckage-wtf-is-this-code-all-features.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/traps_32.c
+++ arch/x86/kernel/traps_32.c
@@ -393,6 +393,8 @@ int __kprobes __die(const char *str, struct pt_regs *regs, long err)
 	unsigned short ss;
 	unsigned long sp;
 
+	ftrace_stop();
+
 	printk(KERN_EMERG "%s: %04lx [#%d] ", str, err & 0xffff, ++die_counter);
 #ifdef CONFIG_PREEMPT
 	printk("PREEMPT ");

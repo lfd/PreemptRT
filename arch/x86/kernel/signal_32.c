5fa3144f62f98385256bf80f7bd23a6a39ea28b5
Thomas Gleixner <tglx@linutronix.de>
2009-01-30 11:43:42 +0000

    x86: remove redundant local_irq_enable() in handle_signal()
    IRQs are already enabled when handle_signal() is called.
    
    Signed-off-by: Carsten Emde <C.Emde@osadl.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/signal_32.c
+++ arch/x86/kernel/signal_32.c
@@ -529,13 +529,6 @@ handle_signal(unsigned long sig, siginfo_t *info, struct k_sigaction *ka,
 		}
 	}
 
-#ifdef CONFIG_PREEMPT_RT
-	/*
-	 * Fully-preemptible kernel does not need interrupts disabled:
-	 */
-	local_irq_enable();
-	preempt_check_resched();
-#endif
 	/*
 	 * If TF is set due to a debugger (TIF_FORCED_TF), clear the TF
 	 * flag so that register information in the sigcontext is correct.

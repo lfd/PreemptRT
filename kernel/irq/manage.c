6a07fac45192a1a2bb6aec99d769f81b63da68c3
Ingo Molnar <mingo@elte.hu>
2009-02-04 00:02:49 +0100

    preempt: realtime irqs
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -661,9 +661,9 @@ void free_irq(unsigned int irq, void *dev_id)
 	 *   'real' IRQ doesn't run in * parallel with our fake. )
 	 */
 	if (action->flags & IRQF_SHARED) {
-		local_irq_save(flags);
+		local_irq_save_nort(flags);
 		action->handler(irq, dev_id);
-		local_irq_restore(flags);
+		local_irq_restore_nort(flags);
 	}
 #endif
 	kfree(action);
@@ -770,11 +770,11 @@ int request_irq(unsigned int irq, irq_handler_t handler,
 		unsigned long flags;
 
 		disable_irq(irq);
-		local_irq_save(flags);
+		local_irq_save_nort(flags);
 
 		handler(irq, dev_id);
 
-		local_irq_restore(flags);
+		local_irq_restore_nort(flags);
 		enable_irq(irq);
 	}
 #endif
@@ -787,6 +787,11 @@ EXPORT_SYMBOL(request_irq);
 int hardirq_preemption = 1;
 
 EXPORT_SYMBOL(hardirq_preemption);
+
+/*
+ * Real-Time Preemption depends on hardirq threading:
+ */
+#ifndef CONFIG_PREEMPT_RT
 
 static int __init hardirq_preempt_setup (char *str)
 {
@@ -802,6 +807,7 @@ static int __init hardirq_preempt_setup (char *str)
 
 __setup("hardirq-preempt=", hardirq_preempt_setup);
 
+#endif
 
 /*
  * threaded simple handler
@@ -961,12 +967,16 @@ static int do_irqd(void * __desc)
 	sys_sched_setscheduler(current->pid, SCHED_FIFO, &param);
 
 	while (!kthread_should_stop()) {
-		local_irq_disable();
+		local_irq_disable_nort();
 		set_current_state(TASK_INTERRUPTIBLE);
+#ifndef CONFIG_PREEMPT_RT
 		irq_enter();
+#endif
 		do_hardirq(desc);
+#ifndef CONFIG_PREEMPT_RT
 		irq_exit();
-		local_irq_enable();
+#endif
+		local_irq_enable_nort();
 		cond_resched();
 #ifdef CONFIG_SMP
 		/*

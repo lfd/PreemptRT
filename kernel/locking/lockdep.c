30edc9df1259da3806dcdf623add9d14b5dd1a49
Thomas Gleixner <tglx@linutronix.de>
2011-07-17 18:51:23 +0200

    lockdep: Make it RT aware
    teach lockdep that we don't really do softirqs on -RT.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -4244,6 +4244,7 @@ static void check_flags(unsigned long flags)
 		}
 	}
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 	/*
 	 * We dont accurately track softirq state in e.g.
 	 * hardirq contexts (such as on 4KSTACKS), so only
@@ -4258,6 +4259,7 @@ static void check_flags(unsigned long flags)
 			DEBUG_LOCKS_WARN_ON(!current->softirqs_enabled);
 		}
 	}
+#endif
 
 	if (!debug_locks)
 		print_irqtrace_events(current);

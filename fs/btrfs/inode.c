3389d363e8603bf1eedbf0a50ecdf06ad47a83a8
Thomas Gleixner <tglx@linutronix.de>
2012-02-27 18:08:46 +0100

    fs: dentry use seqlock
    Replace the open coded seqlock with a real seqlock, so RT can handle
    it.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: stable-rt@vger.kernel.org

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -4002,9 +4002,9 @@ static struct dentry *btrfs_lookup(struct inode *dir, struct dentry *dentry,
 
 	ret = d_splice_alias(btrfs_lookup_dentry(dir, dentry), dentry);
 	if (unlikely(d_need_lookup(dentry))) {
-		spin_lock(&dentry->d_lock);
+		seq_spin_lock(&dentry->d_lock);
 		dentry->d_flags &= ~DCACHE_NEED_LOOKUP;
-		spin_unlock(&dentry->d_lock);
+		seq_spin_unlock(&dentry->d_lock);
 	}
 	return ret;
 }

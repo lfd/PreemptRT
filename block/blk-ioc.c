e4c6868924e2451108661e6a5c87812809c6edb2
Thomas Gleixner <tglx@linutronix.de>
2012-12-20 18:28:26 +0100

    block: Use cpu_chill() for retry loops
    Retry loops on RT might loop forever when the modifying side was
    preempted. Steven also observed a live lock when there was a
    concurrent priority boosting going on.
    
    Use cpu_chill() instead of cpu_relax() to let the system
    make progress.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: stable-rt@vger.kernel.org

--- block/blk-ioc.c
+++ block/blk-ioc.c
@@ -110,7 +110,7 @@ static void ioc_release_fn(struct work_struct *work)
 			spin_unlock(q->queue_lock);
 		} else {
 			spin_unlock_irqrestore(&ioc->lock, flags);
-			cpu_relax();
+			cpu_chill();
 			spin_lock_irqsave_nested(&ioc->lock, flags, 1);
 		}
 	}
@@ -188,7 +188,7 @@ retry:
 			spin_unlock(icq->q->queue_lock);
 		} else {
 			spin_unlock_irqrestore(&ioc->lock, flags);
-			cpu_relax();
+			cpu_chill();
 			goto retry;
 		}
 	}

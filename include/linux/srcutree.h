b5df829d886444ab7718419e58659822730cc9f8
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2013-03-19 14:44:30 +0100

    kernel/SRCU: provide a static initializer
    There are macros for static initializer for the three out of four
    possible notifier types, that are:
    	ATOMIC_NOTIFIER_HEAD()
    	BLOCKING_NOTIFIER_HEAD()
    	RAW_NOTIFIER_HEAD()
    
    This patch provides a static initilizer for the forth type to make it
    complete.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/srcutree.h
+++ include/linux/srcutree.h
@@ -106,9 +106,9 @@ struct srcu_struct {
 
 void process_srcu(struct work_struct *work);
 
-#define __SRCU_STRUCT_INIT(name)					\
+#define __SRCU_STRUCT_INIT(name, pcpu_name)				\
 	{								\
-		.sda = &name##_srcu_data,				\
+		.sda = &pcpu_name,					\
 		.lock = __RAW_SPIN_LOCK_UNLOCKED(name.lock),		\
 		.srcu_gp_seq_needed = 0 - 1,				\
 		__SRCU_DEP_MAP_INIT(name)				\
@@ -135,7 +135,7 @@ void process_srcu(struct work_struct *work);
  */
 #define __DEFINE_SRCU(name, is_static)					\
 	static DEFINE_PER_CPU(struct srcu_data, name##_srcu_data);\
-	is_static struct srcu_struct name = __SRCU_STRUCT_INIT(name)
+	is_static struct srcu_struct name = __SRCU_STRUCT_INIT(name, name##_srcu_data)
 #define DEFINE_SRCU(name)		__DEFINE_SRCU(name, /* not static */)
 #define DEFINE_STATIC_SRCU(name)	__DEFINE_SRCU(name, static)
 

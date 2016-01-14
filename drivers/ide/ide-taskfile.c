7e3457b6abfc62ea2fd05ca345f1b4a0a103f430
Unknown Author <unknown@author.com>
2016-01-14 18:12:17 +0100

    preempt-realtime-ide

--- drivers/ide/ide-taskfile.c
+++ drivers/ide/ide-taskfile.c
@@ -278,7 +278,7 @@ static void ide_pio_sector(ide_drive_t *drive, unsigned int write)
 	offset %= PAGE_SIZE;
 
 #ifdef CONFIG_HIGHMEM
-	local_irq_save(flags);
+	local_irq_save_nort(flags);
 #endif
 	buf = kmap_atomic(page, KM_BIO_SRC_IRQ) + offset;
 
@@ -298,7 +298,7 @@ static void ide_pio_sector(ide_drive_t *drive, unsigned int write)
 
 	kunmap_atomic(buf, KM_BIO_SRC_IRQ);
 #ifdef CONFIG_HIGHMEM
-	local_irq_restore(flags);
+	local_irq_restore_nort(flags);
 #endif
 }
 
@@ -464,7 +464,7 @@ ide_startstop_t pre_task_out_intr (ide_drive_t *drive, struct request *rq)
 	}
 
 	if (!drive->unmask)
-		local_irq_disable();
+		local_irq_disable_nort();
 
 	ide_set_handler(drive, &task_out_intr, WAIT_WORSTCASE, NULL);
 	ide_pio_datablock(drive, rq, 1);

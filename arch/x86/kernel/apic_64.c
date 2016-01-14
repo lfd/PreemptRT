195fade84acb5372a8659247b862d6e43e27631a
Ingo Molnar <mingo@elte.hu>
2016-01-14 18:30:58 +0100

    x86_64: do not enable the NMI watchdog by default
    do not enable the NMI watchdog by default. Now that we have
    lockdep i cannot remember the last time it caught a real bug,
    but the NMI watchdog can /cause/ problems. Furthermore, to the
    typical user, an NMI watchdog assert results in a total lockup
    anyway (if under X). In that sense, all that the NMI watchdog
    does is that it makes the system /less/ stable and /less/
    debuggable.
    
    people can still enable it either after bootup via:
    
       echo 1 > /proc/sys/kernel/nmi
    
    or via the nmi_watchdog=1 or nmi_watchdog=2 boot options.
    
    build and boot tested on an Athlon64 box.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic_64.c
+++ arch/x86/kernel/apic_64.c
@@ -838,7 +838,6 @@ void __cpuinit lapic_setup_esr(void)
 void __cpuinit end_local_APIC_setup(void)
 {
 	lapic_setup_esr();
-	nmi_watchdog_default();
 	setup_apic_nmi_watchdog(NULL);
 	apic_pm_activate();
 }

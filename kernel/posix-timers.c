0fe43d26b74736fc93acfd3440b2618adb01c954
Unknown Author <unknown@author.com>
2016-01-14 18:33:38 +0100

    preempt-irqs-hrtimer
    include/linux/hrtimer.h |   10 ++++++++++
     kernel/hrtimer.c        |   33 ++++++++++++++++++++++++++++++++-
     kernel/itimer.c         |    1 +
     kernel/posix-timers.c   |    3 +++
     4 files changed, 46 insertions(+), 1 deletion(-)

--- kernel/posix-timers.c
+++ kernel/posix-timers.c
@@ -807,6 +807,7 @@ retry:
 
 	unlock_timer(timr, flag);
 	if (error == TIMER_RETRY) {
+		hrtimer_wait_for_timer(&timr->it.real.timer);
 		rtn = NULL;	// We already got the old time...
 		goto retry;
 	}
@@ -846,6 +847,7 @@ retry_delete:
 
 	if (timer_delete_hook(timer) == TIMER_RETRY) {
 		unlock_timer(timer, flags);
+		hrtimer_wait_for_timer(&timer->it.real.timer);
 		goto retry_delete;
 	}
 
@@ -878,6 +880,7 @@ retry_delete:
 
 	if (timer_delete_hook(timer) == TIMER_RETRY) {
 		unlock_timer(timer, flags);
+		hrtimer_wait_for_timer(&timer->it.real.timer);
 		goto retry_delete;
 	}
 	list_del(&timer->list);

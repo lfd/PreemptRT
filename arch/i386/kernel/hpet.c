df64ea67312dffc86c547897c568ae7d5da64424
Unknown Author <unknown@author.com>
2016-01-14 18:11:46 +0100

    latency-tracing-i386
    arch/i386/Kconfig                  |    1 +
     arch/i386/boot/compressed/Makefile |    2 +-
     arch/i386/kernel/Makefile          |    1 +
     arch/i386/kernel/apic.c            |    2 ++
     arch/i386/kernel/entry.S           |   15 +++++++++++++++
     arch/i386/kernel/hpet.c            |    2 +-
     arch/i386/kernel/irq.c             |    7 ++++++-
     arch/i386/kernel/mcount-wrapper.S  |   27 +++++++++++++++++++++++++++
     arch/i386/kernel/traps.c           |    1 +
     arch/i386/kernel/tsc.c             |    2 +-
     arch/i386/mm/fault.c               |    1 +
     arch/i386/mm/init.c                |    2 +-
     include/asm-i386/irq.h             |    2 +-
     include/asm-i386/processor.h       |   11 +++++++----
     14 files changed, 66 insertions(+), 10 deletions(-)

--- arch/i386/kernel/hpet.c
+++ arch/i386/kernel/hpet.c
@@ -292,7 +292,7 @@ static int hpet_legacy_next_event(unsigned long delta,
 /*
  * Clock source related code
  */
-static cycle_t read_hpet(void)
+static cycle_t notrace read_hpet(void)
 {
 	return (cycle_t)hpet_readl(HPET_COUNTER);
 }

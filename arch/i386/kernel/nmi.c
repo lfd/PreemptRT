e694f05b56f37d4291d168bf2def393435bbcad2
Unknown Author <unknown@author.com>
2016-01-14 18:04:44 +0100

    nmi-profiling

--- arch/i386/kernel/nmi.c
+++ arch/i386/kernel/nmi.c
@@ -350,9 +350,9 @@ void nmi_show_all_regs(void)
 	}
 }
 
-static DEFINE_SPINLOCK(nmi_print_lock);
+static DEFINE_RAW_SPINLOCK(nmi_print_lock);
 
-void irq_show_regs_callback(int cpu, struct pt_regs *regs)
+notrace void irq_show_regs_callback(int cpu, struct pt_regs *regs)
 {
 	if (!nmi_show_regs[cpu])
 		return;
@@ -366,7 +366,7 @@ void irq_show_regs_callback(int cpu, struct pt_regs *regs)
 	spin_unlock(&nmi_print_lock);
 }
 
-__kprobes int nmi_watchdog_tick(struct pt_regs *regs, unsigned reason)
+notrace __kprobes int nmi_watchdog_tick(struct pt_regs *regs, unsigned reason)
 {
 	/*
 	 * Since current_thread_info()-> is always on the stack, and we
@@ -433,7 +433,6 @@ __kprobes int nmi_watchdog_tick(struct pt_regs *regs, unsigned reason)
 			printk(KERN_WARNING "NMI watchdog running again ...\n");
 			for_each_online_cpu(i)
 				alert_counter[i] = 0;
-
 
 		}
 

e68316a5189ef1cc92721ecfea4a2497d56fc9ef
Ingo Molnar <mingo@elte.hu>
2016-01-14 18:06:40 +0100

    nmi-driven profiling for /proc/profile
    nmi-driven profiling for /proc/profile
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -439,7 +439,6 @@ static void tick_nohz_handler(struct clock_event_device *dev)
 	}
 
 	update_process_times(user_mode(regs));
-	profile_tick(CPU_PROFILING);
 
 	/* Do not restart, when we are in the idle loop */
 	if (ts->tick_stopped)
@@ -553,7 +552,6 @@ static enum hrtimer_restart tick_sched_timer(struct hrtimer *timer)
 		 */
 		spin_unlock(&base->lock);
 		update_process_times(user_mode(regs));
-		profile_tick(CPU_PROFILING);
 		spin_lock(&base->lock);
 	}
 

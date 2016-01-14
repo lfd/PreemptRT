48428763bc310a498e4c2d93fcd429c0a9a53b8e
Unknown Author <unknown@author.com>
2016-01-14 18:11:15 +0100

    mm/fs: abstract address_space::nrpages
    Currently the tree_lock protects mapping->nrpages, this will not be
    possible much longer. Hence abstract the access to this variable so that
    it can be easily replaced by an atomic_ulong_t.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -225,7 +225,7 @@ extern int end_swap_bio_read(struct bio *bio, unsigned int bytes_done, int err);
 
 /* linux/mm/swap_state.c */
 extern struct address_space swapper_space;
-#define total_swapcache_pages  swapper_space.nrpages
+#define total_swapcache_pages  mapping_nrpages(&swapper_space)
 extern void show_swap_cache_info(void);
 extern int add_to_swap(struct page *, gfp_t);
 extern void __delete_from_swap_cache(struct page *);

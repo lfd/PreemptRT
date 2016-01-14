9655b221e82507f5939487e6d5b3624f7dda0029
Unknown Author <unknown@author.com>
2016-01-14 18:13:48 +0100

    preempt-realtime-sched

--- kernel/sched_rt.c
+++ kernel/sched_rt.c
@@ -106,6 +106,48 @@ static inline void dec_rt_tasks(struct task_struct *p, struct rq *rq)
 #endif /* CONFIG_SMP */
 }
 
+static inline void incr_rt_nr_uninterruptible(struct task_struct *p,
+					      struct rq *rq)
+{
+	rq->rt.rt_nr_uninterruptible++;
+}
+
+static inline void decr_rt_nr_uninterruptible(struct task_struct *p,
+					      struct rq *rq)
+{
+	rq->rt.rt_nr_uninterruptible--;
+}
+
+unsigned long rt_nr_running(void)
+{
+	unsigned long i, sum = 0;
+
+	for_each_online_cpu(i)
+		sum += cpu_rq(i)->rt.rt_nr_running;
+
+	return sum;
+}
+
+unsigned long rt_nr_running_cpu(int cpu)
+{
+	return cpu_rq(cpu)->rt.rt_nr_running;
+}
+
+unsigned long rt_nr_uninterruptible(void)
+{
+	unsigned long i, sum = 0;
+
+	for_each_online_cpu(i)
+		sum += cpu_rq(i)->rt.rt_nr_uninterruptible;
+
+	return sum;
+}
+
+unsigned long rt_nr_uninterruptible_cpu(int cpu)
+{
+	return cpu_rq(cpu)->rt.rt_nr_uninterruptible;
+}
+
 static void enqueue_task_rt(struct rq *rq, struct task_struct *p, int wakeup)
 {
 	struct rt_prio_array *array = &rq->rt.active;
@@ -114,6 +156,9 @@ static void enqueue_task_rt(struct rq *rq, struct task_struct *p, int wakeup)
 	__set_bit(p->prio, array->bitmap);
 
 	inc_rt_tasks(p, rq);
+
+	if (p->state == TASK_UNINTERRUPTIBLE)
+		decr_rt_nr_uninterruptible(p, rq);
 }
 
 /*
@@ -124,6 +169,9 @@ static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int sleep)
 	struct rt_prio_array *array = &rq->rt.active;
 
 	update_curr_rt(rq);
+
+	if (p->state == TASK_UNINTERRUPTIBLE)
+		incr_rt_nr_uninterruptible(p, rq);
 
 	list_del(&p->run_list);
 	if (list_empty(array->queue + p->prio))
@@ -546,6 +594,8 @@ static int push_rt_task(struct rq *rq)
 
 	resched_task(lowest_rq->curr);
 
+	schedstat_inc(rq, rto_pushed);
+
 	spin_unlock(&lowest_rq->lock);
 
 	ret = 1;
@@ -698,6 +748,7 @@ static int pull_rt_task(struct rq *this_rq)
 			 */
 			next = p;
 
+			schedstat_inc(src_rq, rto_pulled);
 		}
  bail:
 		spin_unlock(&src_rq->lock);
@@ -711,8 +762,10 @@ static void schedule_balance_rt(struct rq *rq,
 {
 	/* Try to pull RT tasks here if we lower this rq's prio */
 	if (unlikely(rt_task(prev)) &&
-	    rq->rt.highest_prio > prev->prio)
+	    rq->rt.highest_prio > prev->prio) {
 		pull_rt_task(rq);
+		schedstat_inc(rq, rto_schedule);
+	}
 }
 
 static void schedule_tail_balance_rt(struct rq *rq)
@@ -727,6 +780,7 @@ static void schedule_tail_balance_rt(struct rq *rq)
 	if (unlikely(rq->rt.overloaded)) {
 		spin_lock_irq(&rq->lock);
 		push_rt_tasks(rq);
+		schedstat_inc(rq, rto_schedule_tail);
 		spin_unlock_irq(&rq->lock);
 	}
 }
@@ -736,8 +790,10 @@ static void wakeup_balance_rt(struct rq *rq, struct task_struct *p)
 	if (unlikely(rt_task(p)) &&
 	    !task_running(rq, p) &&
 	    (p->prio >= rq->rt.highest_prio) &&
-	    rq->rt.overloaded)
+	    rq->rt.overloaded) {
 		push_rt_tasks(rq);
+ 		schedstat_inc(rq, rto_wakeup);
+	}
 }
 
 static void set_cpus_allowed_rt(struct task_struct *p, cpumask_t *new_mask)

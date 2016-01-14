586b8c2dc8a51d75b67e3f82c182f91f6aafacac
Unknown Author <unknown@author.com>
2016-01-14 18:30:47 +0100

    tracer-add-event-markers
    Add markers to various events
    
    This patch adds markers to various events in the kernel.
    (interrupts, task activation and hrtimers)
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>

--- kernel/hrtimer.c
+++ kernel/hrtimer.c
@@ -44,6 +44,8 @@
 #include <linux/seq_file.h>
 #include <linux/err.h>
 
+#include <linux/ftrace.h>
+
 #include <asm/uaccess.h>
 
 /**
@@ -735,6 +737,7 @@ static void enqueue_hrtimer(struct hrtimer *timer,
 	struct hrtimer *entry;
 	int leftmost = 1;
 
+	ftrace_event_timer(&timer->expires, timer);
 	/*
 	 * Find the right place in the rbtree:
 	 */
@@ -1169,6 +1172,7 @@ void hrtimer_interrupt(struct clock_event_device *dev)
 
  retry:
 	now = ktime_get();
+	ftrace_event_timestamp(&now);
 
 	expires_next.tv64 = KTIME_MAX;
 
@@ -1206,6 +1210,8 @@ void hrtimer_interrupt(struct clock_event_device *dev)
 				raise = 1;
 				continue;
 			}
+
+			ftrace_event_timer(&timer->expires, timer);
 
 			__run_hrtimer(timer);
 		}

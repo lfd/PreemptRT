055e7875be83c92993185838cf2b7f98cc4ce539
Unknown Author <unknown@author.com>
2016-01-14 18:31:36 +0100

    rt-mutex-i386

--- arch/x86/kernel/i386_ksyms_32.c
+++ arch/x86/kernel/i386_ksyms_32.c
@@ -4,10 +4,12 @@
 #include <asm/desc.h>
 #include <asm/pgtable.h>
 
-EXPORT_SYMBOL(__down_failed);
-EXPORT_SYMBOL(__down_failed_interruptible);
-EXPORT_SYMBOL(__down_failed_trylock);
-EXPORT_SYMBOL(__up_wakeup);
+#ifdef CONFIG_ASM_SEMAPHORES
+EXPORT_SYMBOL(__compat_down_failed);
+EXPORT_SYMBOL(__compat_down_failed_interruptible);
+EXPORT_SYMBOL(__compat_down_failed_trylock);
+EXPORT_SYMBOL(__compat_up_wakeup);
+#endif
 /* Networking helper routines. */
 EXPORT_SYMBOL(csum_partial_copy_generic);
 

7c57c403a3132d2a9f4ccefd44c7420b56cf7410
Unknown Author <unknown@author.com>
2016-01-14 18:07:40 +0100

    i386/x8664: cleanup the shared hpet code
    Remove hpet_readl/writel from vsyscall.h, where it does not belong
    anyway. Use the hpet code itself.
    
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-i386/hpet.h
+++ include/asm-i386/hpet.h
@@ -66,6 +66,7 @@
 extern unsigned long hpet_address;
 extern int is_hpet_enabled(void);
 extern int hpet_enable(void);
+extern unsigned long hpet_readl(unsigned long a);
 
 #ifdef CONFIG_HPET_EMULATE_RTC
 
@@ -85,6 +86,7 @@ extern irqreturn_t hpet_rtc_interrupt(int irq, void *dev_id);
 #else
 
 static inline int hpet_enable(void) { return 0; }
+static inline unsigned long hpet_readl(unsigned long a) { return 0; }
 
 #endif /* CONFIG_HPET_TIMER */
 #endif /* _I386_HPET_H */

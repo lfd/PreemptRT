04fb5775f008c4dab0dcf02ee7fee6992365dc51
Unknown Author <unknown@author.com>
2016-01-14 18:10:58 +0100

    rt: rename rt_mutex_setprio to task_setprio
    With there being multiple non-mutex users of this function its past time it
    got renamed.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- kernel/sched.c
+++ kernel/sched.c
@@ -4192,10 +4192,8 @@ long __sched sleep_on_timeout(wait_queue_head_t *q, long timeout)
 }
 EXPORT_SYMBOL(sleep_on_timeout);
 
-#ifdef CONFIG_RT_MUTEXES
-
 /*
- * rt_mutex_setprio - set the current priority of a task
+ * task_setprio - set the current priority of a task
  * @p: task
  * @prio: prio value (kernel-internal form)
  *
@@ -4204,7 +4202,7 @@ EXPORT_SYMBOL(sleep_on_timeout);
  *
  * Used by the rt_mutex code to implement priority inheritance logic.
  */
-void rt_mutex_setprio(struct task_struct *p, int prio)
+void task_setprio(struct task_struct *p, int prio)
 {
 	unsigned long flags;
 	int oldprio, prev_resched, on_rq;
@@ -4268,8 +4266,6 @@ void rt_mutex_setprio(struct task_struct *p, int prio)
 out_unlock:
 	task_rq_unlock(rq, &flags);
 }
-
-#endif
 
 void set_user_nice(struct task_struct *p, long nice)
 {

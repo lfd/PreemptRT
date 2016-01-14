6471c67ce2d286e6ac23b1d8bb1188d48d51b962
Venki Pallipadi <venkatesh.pallipadi@intel.com>
2016-01-14 18:06:17 +0100

    ich-force-hpet-late-initialization-of-hpet-after-quirk
    Enable HPET later during boot, after the force detect in PCI quirks.  Also add
    a call to repeat the force enabling at resume time.
    
    Signed-off-by: Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Andi Kleen <ak@suse.de>
    Cc: john stultz <johnstul@us.ibm.com>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/asm-i386/hpet.h
+++ include/asm-i386/hpet.h
@@ -64,6 +64,7 @@
 
 /* hpet memory map physical address */
 extern unsigned long hpet_address;
+extern unsigned long force_hpet_address;
 extern int is_hpet_enabled(void);
 extern int hpet_enable(void);
 extern unsigned long hpet_readl(unsigned long a);

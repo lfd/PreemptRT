aac1de0650860884ac3517714653a2d6eb8aa32c
Unknown Author <unknown@author.com>
2016-01-14 18:14:29 +0100

    hrtimer-trace
    include/linux/hrtimer.h   |    6 ++++++
     kernel/hrtimer.c          |    5 +++++
     kernel/time/clockevents.c |    4 ++++
     3 files changed, 15 insertions(+)

--- kernel/hrtimer.c
+++ kernel/hrtimer.c
@@ -706,6 +706,8 @@ static void enqueue_hrtimer(struct hrtimer *timer,
 	struct hrtimer *entry;
 	int leftmost = 1;
 
+	hrtimer_trace(timer->expires, (unsigned long) timer);
+
 	/*
 	 * Find the right place in the rbtree:
 	 */
@@ -1047,6 +1049,7 @@ void hrtimer_interrupt(struct clock_event_device *dev)
 
  retry:
 	now = ktime_get();
+	hrtimer_trace(now, 0);
 
 	expires_next.tv64 = KTIME_MAX;
 
@@ -1074,6 +1077,8 @@ void hrtimer_interrupt(struct clock_event_device *dev)
 					expires_next = expires;
 				break;
 			}
+
+			hrtimer_trace(timer->expires, (unsigned long) timer);
 
 			/* Move softirq callbacks to the pending list */
 			if (timer->cb_mode == HRTIMER_CB_SOFTIRQ) {

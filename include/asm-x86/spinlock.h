5863265e258d549ef54956823d91f3a317cb9bbe
Unknown Author <unknown@author.com>
2016-01-14 18:34:32 +0100

    preempt-realtime-x86_64
    arch/x86/kernel/early_printk.c |    2 +-
     arch/x86/kernel/head64.c       |    6 +++++-
     arch/x86/kernel/i8259_64.c     |    2 +-
     arch/x86/kernel/io_apic_64.c   |   13 +++++++------
     arch/x86/kernel/nmi_64.c       |    2 ++
     arch/x86/kernel/process_64.c   |    9 ++++++---
     arch/x86/kernel/signal_64.c    |    7 +++++++
     arch/x86/kernel/smp.c          |   12 +++++++++++-
     arch/x86/kernel/tlb_64.c       |    2 +-
     arch/x86/kernel/traps_64.c     |    5 ++---
     include/asm-x86/acpi.h         |    4 ++--
     include/asm-x86/hw_irq_64.h    |    2 +-
     include/asm-x86/i8259.h        |    2 +-
     include/asm-x86/spinlock.h     |    6 +++---
     include/asm-x86/tlbflush.h     |    2 ++
     include/asm-x86/vgtod.h        |    2 +-
     16 files changed, 53 insertions(+), 25 deletions(-)

--- include/asm-x86/spinlock.h
+++ include/asm-x86/spinlock.h
@@ -274,8 +274,8 @@ static inline void __raw_write_unlock(__raw_rwlock_t *rw)
 		     : "+m" (rw->lock) : "i" (RW_LOCK_BIAS) : "memory");
 }
 
-#define _raw_spin_relax(lock)	cpu_relax()
-#define _raw_read_relax(lock)	cpu_relax()
-#define _raw_write_relax(lock)	cpu_relax()
+#define __raw_spin_relax(lock)	cpu_relax()
+#define __raw_read_relax(lock)	cpu_relax()
+#define __raw_write_relax(lock)	cpu_relax()
 
 #endif

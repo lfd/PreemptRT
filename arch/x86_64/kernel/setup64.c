cda259da7bd90f59c6b87c8f5d0e1a36e1ca612b
Unknown Author <unknown@author.com>
2016-01-14 18:04:32 +0100

    latency-tracing-x86_64
    arch/x86_64/ia32/ia32entry.S  |   11 ++++++++-
     arch/x86_64/kernel/entry.S    |   45 +++++++++++++++++++++++++++++++++++++
     arch/x86_64/kernel/head64.c   |    3 +-
     arch/x86_64/kernel/irq.c      |    6 +++++
     arch/x86_64/kernel/setup64.c  |    4 +--
     arch/x86_64/kernel/smpboot.c  |    2 -
     arch/x86_64/kernel/traps.c    |    1 
     arch/x86_64/kernel/vsyscall.c |    2 -
     include/asm-x86_64/calling.h  |   50 ++++++++++++++++++++++++++++++++++++++++++
     include/asm-x86_64/unistd.h   |    2 +
     10 files changed, 120 insertions(+), 6 deletions(-)

--- arch/x86_64/kernel/setup64.c
+++ arch/x86_64/kernel/setup64.c
@@ -114,7 +114,7 @@ void __init setup_per_cpu_areas(void)
 	}
 } 
 
-void pda_init(int cpu)
+void notrace pda_init(int cpu)
 { 
 	struct x8664_pda *pda = cpu_pda(cpu);
 
@@ -191,7 +191,7 @@ unsigned long kernel_eflags;
  * 'CPU state barrier', nothing should get across.
  * A lot of state is already set up in PDA init.
  */
-void __cpuinit cpu_init (void)
+void __cpuinit notrace cpu_init (void)
 {
 	int cpu = stack_smp_processor_id();
 	struct tss_struct *t = &per_cpu(init_tss, cpu);

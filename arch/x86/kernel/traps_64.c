4ea595ec01102a27a936fb53f932032c1d99c97d
Unknown Author <unknown@author.com>
2016-01-14 18:35:50 +0100

    preempt-realtime-x86_64
    arch/x86/kernel/early_printk.c |    2 +-
     arch/x86/kernel/head64.c       |    6 +++++-
     arch/x86/kernel/i8259_64.c     |    2 +-
     arch/x86/kernel/io_apic_64.c   |   13 +++++++------
     arch/x86/kernel/nmi_64.c       |    2 ++
     arch/x86/kernel/process_64.c   |    9 ++++++---
     arch/x86/kernel/signal_64.c    |    7 +++++++
     arch/x86/kernel/smp.c          |   12 +++++++++++-
     arch/x86/kernel/tlb_64.c       |    2 +-
     arch/x86/kernel/traps_64.c     |    5 ++---
     include/asm-x86/acpi.h         |    4 ++--
     include/asm-x86/hw_irq_64.h    |    2 +-
     include/asm-x86/i8259.h        |    2 +-
     include/asm-x86/spinlock.h     |    6 +++---
     include/asm-x86/tlbflush.h     |    2 ++
     include/asm-x86/vgtod.h        |    2 +-
     16 files changed, 53 insertions(+), 25 deletions(-)

--- arch/x86/kernel/traps_64.c
+++ arch/x86/kernel/traps_64.c
@@ -263,7 +263,7 @@ void dump_trace(struct task_struct *tsk, struct pt_regs *regs,
 		unsigned long *stack, unsigned long bp,
 		const struct stacktrace_ops *ops, void *data)
 {
-	const unsigned cpu = get_cpu();
+	const unsigned cpu = raw_smp_processor_id();
 	unsigned long *irqstack_end = (unsigned long*)cpu_pda(cpu)->irqstackptr;
 	unsigned used = 0;
 	struct thread_info *tinfo;
@@ -347,7 +347,6 @@ void dump_trace(struct task_struct *tsk, struct pt_regs *regs,
 	 * This handles the process stack:
 	 */
 	bp = print_context_stack(tinfo, stack, bp, ops, data, NULL);
-	put_cpu();
 }
 EXPORT_SYMBOL(dump_trace);
 
@@ -399,7 +398,7 @@ _show_stack(struct task_struct *tsk, struct pt_regs *regs, unsigned long *sp,
 {
 	unsigned long *stack;
 	int i;
-	const int cpu = smp_processor_id();
+	const int cpu = raw_smp_processor_id();
 	unsigned long *irqstack_end = (unsigned long *) (cpu_pda(cpu)->irqstackptr);
 	unsigned long *irqstack = (unsigned long *) (cpu_pda(cpu)->irqstackptr - IRQSTACKSIZE);
 

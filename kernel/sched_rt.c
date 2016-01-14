cb1053fd1d946ec119eb67d78514f2969986f105
Unknown Author <unknown@author.com>
2016-01-14 18:11:21 +0100

    rt-overload
    RT overloaded runqueues accounting
    
    This patch adds an RT overload accounting system. When a runqueue has
    more than one RT task queued, it is marked as overloaded. That is that it
    is a candidate to have RT tasks pulled from it.
    
    TODO for 2.6.24-rc1:
    If CONFIG_CPUSET is defined, then an rt overloaded CPU bitmask is created
    in the cpusets.  The algorithm for pulling tasks is limited to the cpuset
    of the current task on the runqueue. Because of overlapping cpusets, it is
    possible that the bitmask may get out of sync with actual overloaded RT
    runqueues. But it wont cause any real harm. The worst that can happen is
    that a RT task may not migrate to a CPU that it can run on when it could.
    But that's a OK price to pay to keep the accounting simple and not kill
    the cache on large SMP boxes.
    
    When CONFIG_CPUSET is not set, then a single RT overload CPU mask is used.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/sched_rt.c
+++ kernel/sched_rt.c
@@ -3,6 +3,24 @@
  * policies)
  */
 
+static cpumask_t rt_overload_mask;
+static inline int rt_overloaded(struct task_struct *p)
+{
+	return !cpus_empty(rt_overload_mask);
+}
+static inline cpumask_t *rt_overload(struct task_struct *p)
+{
+	return &rt_overload_mask;
+}
+static inline void rt_set_overload(struct task_struct *p, int cpu)
+{
+	cpu_set(cpu, rt_overload_mask);
+}
+static inline void rt_clear_overload(struct task_struct *p, int cpu)
+{
+	cpu_clear(cpu, rt_overload_mask);
+}
+
 /*
  * Update the current task's runtime statistics. Skip current tasks that
  * are not in our scheduling class.
@@ -32,6 +50,8 @@ static inline void inc_rt_tasks(struct task_struct *p, struct rq *rq)
 #ifdef CONFIG_SMP
 	if (p->prio < rq->rt.highest_prio)
 		rq->rt.highest_prio = p->prio;
+	if (rq->rt.rt_nr_running > 1)
+		rt_set_overload(p, rq->cpu);
 #endif /* CONFIG_SMP */
 }
 
@@ -53,6 +73,8 @@ static inline void dec_rt_tasks(struct task_struct *p, struct rq *rq)
 		} /* otherwise leave rq->highest prio alone */
 	} else
 		rq->rt.highest_prio = MAX_RT_PRIO;
+	if (rq->rt.rt_nr_running < 2)
+		rt_clear_overload(p, rq->cpu);
 #endif /* CONFIG_SMP */
 }
 

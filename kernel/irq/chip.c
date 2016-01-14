3d1db56b7fdf623b70d83d708442d0a6f0c7d980
Jarek Poplawski <jarkao2@o2.pl>
2016-01-14 18:41:31 +0100

    genirq: fix simple and fasteoi irq handlers
    After the "genirq: do not mask interrupts by default" patch interrupts
    should be disabled not immediately upon request, but after they happen.
    But, handle_simple_irq() and handle_fasteoi_irq() can skip this once or
    more if an irq is just serviced (IRQ_INPROGRESS), possibly disrupting a
    driver's work.
    
    The main reason of problems here, pointing the broken patch and making
    the first patch which can fix this was done by Marcin Slusarz.
    Additional test patches of Thomas Gleixner and Ingo Molnar tested by
    Marcin Slusarz helped to narrow possible reasons even more. Thanks.
    
    PS: this patch fixes only one evident error here, but there could be
    more places affected by above-mentioned change in irq handling.
    
    PS 2:
    After rethinking, IMHO, there are two most probable scenarios here:
    
    1. After hw resend there could be a conflict between retriggered
    edge type irq and the next level type one: e.g. if this level type
    irq (io_apic is enabled then) is triggered while retriggered irq is
    serviced (IRQ_INPROGRESS) there is goto out with eoi, and probably
    the next such levels are triggered and looping, so probably kind of
    flood in io_apic until this retriggered edge service has ended.
    2. There is something wrong with ioapic_retrigger_irq (less probable
    because this should be probably seen with 'normal' edge retriggers,
    but on the other hand, they could be less common).
    
    So, if there is #1, this fixed patch should work.
    
    But, since level types don't need this retriggers too much I think
    this "don't mask interrupts by default" idea should be rethinked:
    is there enough gain to risk such hard to diagnose errors?
    
    So, IMHO, there should be at least possibility to turn this off for
    level types in config (it should be a visible option, so people could
    find & try this before writing for help or changing a network card).
    
    Signed-off-by: Jarek Poplawski <jarkao2@o2.pl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -346,6 +346,8 @@ handle_simple_irq(unsigned int irq, struct irq_desc *desc)
 
 	spin_lock(&desc->lock);
 	desc->status &= ~IRQ_INPROGRESS;
+	if (!(desc->status & IRQ_DISABLED) && desc->chip->unmask)
+		desc->chip->unmask(irq);
 out_unlock:
 	spin_unlock(&desc->lock);
 }
@@ -424,18 +426,16 @@ handle_fasteoi_irq(unsigned int irq, struct irq_desc *desc)
 
 	spin_lock(&desc->lock);
 
-	if (unlikely(desc->status & IRQ_INPROGRESS))
-		goto out;
-
 	desc->status &= ~(IRQ_REPLAY | IRQ_WAITING);
 	kstat_incr_irqs_this_cpu(irq, desc);
 
 	/*
-	 * If its disabled or no action available
+	 * If it's running, disabled or no action available
 	 * then mask it and get out of here:
 	 */
 	action = desc->action;
-	if (unlikely(!action || (desc->status & IRQ_DISABLED))) {
+	if (unlikely(!action || (desc->status & (IRQ_INPROGRESS |
+						 IRQ_DISABLED)))) {
 		desc->status |= IRQ_PENDING;
 		if (desc->chip->mask)
 			desc->chip->mask(irq);
@@ -461,6 +461,8 @@ handle_fasteoi_irq(unsigned int irq, struct irq_desc *desc)
 
 	spin_lock(&desc->lock);
 	desc->status &= ~IRQ_INPROGRESS;
+	if (!(desc->status & IRQ_DISABLED) && desc->chip->unmask)
+		desc->chip->unmask(irq);
 out:
 	desc->chip->eoi(irq);
 	desc = irq_remap_to_desc(irq, desc);

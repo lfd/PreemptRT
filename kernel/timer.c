23c1dc6a47b66d97ec83c5abbc7af84447d69c8a
Unknown Author <unknown@author.com>
2016-01-14 18:07:15 +0100

    rt-time-starvation-fix
    Hey Ingo,
    	Noticed -rt has been updated a few times and this is still missing so I
    figured I'd resend it just in case you missed it:
    
    	We've worked around this before, but its cropped up again. Since
    update_wall_time is now called from a softirq, it can be preempted by a
    high priority process. If its preempted for long enough, the clocksource
    can wrap, causing time to stop incrementing, which if the preempting
    process is checking the time, can cause a hard lockup.
    
    This patch forces the clocksource to be read each tick, and accumulate
    only the cycle count. This allows the update_wall_time to be deferred
    w/o fear of hardware overflow.
    
    thanks
    -john
    
     arch/x86_64/kernel/vsyscall.c |    5 ++++-
     include/linux/clocksource.h   |   40 ++++++++++++++++++++++++++++++++++++++--
     include/linux/time.h          |    1 +
     kernel/time/timekeeping.c     |   34 ++++++++++++++++++----------------
     kernel/timer.c                |    1 +
     5 files changed, 62 insertions(+), 19 deletions(-)
    
    linux-2.6.21-rc5_cycles-accumulated_C7.patch
    ============================================

--- kernel/timer.c
+++ kernel/timer.c
@@ -1031,6 +1031,7 @@ static void run_timer_softirq(struct softirq_action *h)
 void do_timer(unsigned long ticks)
 {
 	jiffies_64 += ticks;
+	timekeeping_accumulate();
 }
 
 #ifdef __ARCH_WANT_SYS_ALARM

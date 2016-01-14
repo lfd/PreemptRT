94742ef23dd7912d6c1d6748e914ebb570b89b7c
Ingo Molnar <mingo@elte.hu>
2016-01-14 18:31:00 +0100

    introduce pause_on_oops_head/tail boot options
    if a system crashes with hard to debug oopses which scroll off the
    screen then it's useful to stop the crash right after the register
    info or right after the callback printout.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/traps_32.c
+++ arch/x86/kernel/traps_32.c
@@ -287,9 +287,15 @@ static void show_stack_log_lvl(struct task_struct *task, struct pt_regs *regs,
 			printk("\n%s       ", log_lvl);
 		printk("%08lx ", *stack++);
 	}
+
+	pause_on_oops_head();
+
 	printk("\n%sCall Trace:\n", log_lvl);
 	show_trace_log_lvl(task, regs, sp, bp, log_lvl);
 	debug_show_held_locks(task);
+
+	pause_on_oops_tail();
+
 }
 
 void show_stack(struct task_struct *task, unsigned long *sp)

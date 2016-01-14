c8aad97a8e52bd4d609803fcca48fda99c61bc62
Unknown Author <unknown@author.com>
2016-01-14 18:36:20 +0100

    mm/fs: abstract address_space::nrpages
    Currently the tree_lock protects mapping->nrpages, this will not be
    possible much longer. Hence abstract the access to this variable so that
    it can be easily replaced by an atomic_ulong_t.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- fs/drop_caches.c
+++ fs/drop_caches.c
@@ -20,7 +20,7 @@ static void drop_pagecache_sb(struct super_block *sb)
 	list_for_each_entry(inode, &sb->s_inodes, i_sb_list) {
 		if (inode->i_state & (I_FREEING|I_WILL_FREE))
 			continue;
-		if (inode->i_mapping->nrpages == 0)
+		if (mapping_nrpages(inode->i_mapping) == 0)
 			continue;
 		__iget(inode);
 		spin_unlock(&inode_lock);

16ae81ea9445dfb666c7e25289377d6269aec47b
Thomas Gleixner <tglx@linutronix.de>
2017-07-12 09:50:11 +0200

    locking/rt/rwlock: Provide reader biased rwlock for RT
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/rwlock_types_rt.h
+++ include/linux/rwlock_types_rt.h
@@ -29,4 +29,46 @@ typedef struct {
 #define DEFINE_RWLOCK(name) \
 	rwlock_t name = __RW_LOCK_UNLOCKED(name)
 
+#define READER_BIAS	(1U << 31)
+#define WRITER_BIAS	(1U << 30)
+
+/*
+ * A reader biased implementation primarily for CPU pinning.
+ *
+ * Can be selected as general replacement for the single reader RT rwlock
+ * variant
+ */
+struct rt_rw_lock {
+	struct rt_mutex		rtmutex;
+	atomic_t		readers;
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+	struct lockdep_map	dep_map;
+#endif
+};
+
+#define __RWLOCK_RT_INITIALIZER(name)					\
+{									\
+	.readers = ATOMIC_INIT(READER_BIAS),				\
+	.rtmutex = __RT_MUTEX_INITIALIZER_SAVE_STATE(name.rtmutex),	\
+	RW_DEP_MAP_INIT(name)						\
+}
+
+void __rwlock_biased_rt_init(struct rt_rw_lock *lock, const char *name,
+			     struct lock_class_key *key);
+
+#define rwlock_biased_rt_init(rwlock)					\
+	do {								\
+		static struct lock_class_key __key;			\
+									\
+		__rwlock_biased_rt_init((rwlock), #rwlock, &__key);	\
+	} while (0)
+
+int __read_rt_trylock(struct rt_rw_lock *rwlock);
+void __read_rt_lock(struct rt_rw_lock *rwlock);
+void __read_rt_unlock(struct rt_rw_lock *rwlock);
+
+void __write_rt_lock(struct rt_rw_lock *rwlock);
+int __write_rt_trylock(struct rt_rw_lock *rwlock);
+void __write_rt_unlock(struct rt_rw_lock *rwlock);
+
 #endif

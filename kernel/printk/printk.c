1b0796e58b28003a36af9ac6b9aee3896f314b89
Julia Lawall <julia.lawall@inria.fr>
2020-07-24 12:05:31 +0200

    printk: fix ifnullfree.cocci warnings
    Make the code a little simpler by dropping
    some unneeded tests.
    
    Generated by: scripts/coccinelle/free/ifnullfree.cocci
    
    Fixes: c406fbce2054 ("printk: implement syslog")
    CC: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Acked-by: John Ogness <john.ogness@linutronix.de>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1458,10 +1458,8 @@ static int syslog_print_all(char __user *buf, int size, bool clear)
 	if (clear && !seq)
 		syslog_clear();
 
-	if (text)
-		kfree(text);
-	if (msgbuf)
-		kfree(msgbuf);
+	kfree(text);
+	kfree(msgbuf);
 	return len;
 }
 
@@ -1614,10 +1612,8 @@ int do_syslog(int type, char __user *buf, int len, int source)
 		break;
 	}
 out:
-	if (msgbuf)
-		kfree(msgbuf);
-	if (text)
-		kfree(text);
+	kfree(msgbuf);
+	kfree(text);
 	return error;
 }
 
